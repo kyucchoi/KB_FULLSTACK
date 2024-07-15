404b8ede305681e1923042f0c9b5f2ac099b27b9
kyucchoi <kyucchoi@naver.com>
2024-07-15 21:12:05 +0900

    20240715

--- 04_JAVA/afternoon/src/abstractt/Duck.java
+++ 04_JAVA/afternoon/src/abstractt/Duck.java
@@ -3,15 +3,15 @@ package abstractt;
 public class Duck extends AbstractAnimal {
     @Override
     public void sound() {
-        System.out.println("오리는 꽥꽥~!");
+        System.out.println("오리는 꽥꽥!");
     }
 
     @Override
     public void eat() {
-        System.out.println("오리는 도토리를 먹어요~!");
+        System.out.println("오리는 도토리를 먹어요!");
     }
     
     public void fly() {
-        System.out.println("오리 날다~!");
+        System.out.println("오리 날다~~!");
     }
 }
