a0a1942c43e9af69c4804caba9d4dee3872d37f9
kyucchoi <kyucchoi@naver.com>
2024-08-03 10:39:04 +0900

    20240803

--- EXTRA/selfStudy/src/level1/문자열다루기기본/Solution.java
+++ EXTRA/selfStudy/src/level1/문자열다루기기본/Solution.java
@@ -2,8 +2,13 @@ package level1.문자열다루기기본;
 
 public class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
-        return answer;
+        if (s.length() != 4 && s.length() != 6) return false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') return false;
+        }
+
+        return true;
     }
 }
 
