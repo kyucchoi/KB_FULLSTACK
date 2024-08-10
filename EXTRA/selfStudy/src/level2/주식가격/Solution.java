1be3710f189f536a2c03a9167c18472ac52452de
kyucchoi <kyucchoi@naver.com>
2024-08-11 00:48:06 +0900

    20240811

---
+++ EXTRA/selfStudy/src/level2/주식가격/Solution.java
@@ -0,0 +1,19 @@
+package level2.주식가격;
+
+public class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
