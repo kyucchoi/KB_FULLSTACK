13dbe0598ab20074776d8e1fbf75033b2ab3ec9b
kyucchoi <kyucchoi@naver.com>
2024-08-19 01:48:17 +0900

    20240819

--- 04_JAVA/algorithm/codingTest/src/study240816/pathWithMaximumProbability/Solution.java
+++ 04_JAVA/algorithm/codingTest/src/study240816/pathWithMaximumProbability/Solution.java
@@ -4,20 +4,18 @@ import java.util.*;
 
 public class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        //✅ 인풋을 본인이 쓰기 편한 구조로 바꾸기 => 무향 그래프 만들기
         Map<Integer, List<Edge>> graph = new HashMap<>();
         double[] probability = new double[n];
+
         for (int i = 0; i < edges.length; i++) {
             graph.putIfAbsent(edges[i][0], new ArrayList<>());
             graph.putIfAbsent(edges[i][1], new ArrayList<>());
             graph.get(edges[i][0]).add(new Edge(edges[i][1], succProb[i]));
             graph.get(edges[i][1]).add(new Edge(edges[i][0], succProb[i]));
         }
-        //✅ 시작지점을 우선순위 큐에 삽입한다.
         Queue<Entry> pq = new PriorityQueue<>();
         pq.add(new Entry(start_node, 1.0));
         probability[start_node] = 1.0;
-        //✅ 확률을 기준으로 다익스트라를 진행한다.
         while (!pq.isEmpty()) {
             Entry cur = pq.remove();
             if (probability[cur.node] > cur.prob) continue;
@@ -26,13 +24,12 @@ public class Solution {
             for (Edge edge : graph.get(cur.node)) {
                 double nextProb = cur.prob * edge.weight;
                 if (probability[edge.node] < nextProb) {
-                    //✅ 다익스트라 진행 중 각 노드에 도달하는 경로의 확률값을 저장한다.
                     probability[edge.node] = nextProb;
                     pq.add(new Entry(edge.node, nextProb));
                 }
             }
         }
-        //✅ end 노드의 확률값을 반환한다.
+
         return probability[end_node];
     }
 
--- 04_JAVA/algorithm/codingTest/src/study240816/합승택시요금/Solution.java
+++ 04_JAVA/algorithm/codingTest/src/study240816/합승택시요금/Solution.java
@@ -5,21 +5,19 @@ import java.util.*;
 public class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
         int answer = 0;
-        //✅ 인풋을 본인이 쓰기 편한 구조로 바꾸기(다익스트라) => 무방향 그래프 만들기
         int[][] costs = new int[n + 1][n + 1];
-        
+
         for (int[] fare : fares) {
             costs[fare[0]][fare[1]] = fare[2];
             costs[fare[1]][fare[0]] = fare[2];
         }
-        //✅ s, a, b 3개의 노드에서 모든 노드까지 도달하는 최소 비용을 저장한다.
+
         int[][] distArray = new int[3][n + 1];
 
         for (int[] d : distArray) {
             Arrays.fill(d, Integer.MAX_VALUE);
         }
 
-        //✅ s, a, b 3개의 노드에서 각각 다익스트라 알고리즘을 수행한다.
         Queue<int[]> pq = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
         final int[] start = {s, a, b};
 
@@ -42,7 +40,7 @@ public class Solution {
                 }
             }
         }
-        //✅ 모든 노드를 순회하며 cost(s->x) + cost(x->a) + cost(x->b)의 최소비용을 반환한다.
+
         int minCost = Integer.MAX_VALUE;
 
         for (int i = 1; i <= n; i++) {
