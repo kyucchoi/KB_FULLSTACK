b7d27aa1b18d981cbdf55b842c47b83806e3ad12
kyucchoi <kyucchoi@naver.com>
2024-09-03 21:51:44 +0900

    20240903

---
+++ 08_SPRING/spring-practice/src/main/java/org/example/board/controller/BoardRestController.java
@@ -0,0 +1,51 @@
+package org.example.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.board.domain.Board;
+import org.example.board.service.BoardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest/board")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+@Slf4j
+public class BoardRestController {
+    private final BoardService service;
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Board>> list() {
+        List<Board> list = service.getList();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Board> get(@PathVariable("id") Long id) {
+        Board board = service.detail(id);
+        return ResponseEntity.ok(board);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> create(@RequestBody Board board) {
+        service.create(board);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<String> update(@RequestBody Board board) {
+        System.out.println(board.toString());
+        service.update(board);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<String> delete(@RequestParam("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+}
---
+++ 08_SPRING/spring-practice/src/main/java/org/example/board/service/BoardServiceImpl.java
@@ -0,0 +1,45 @@
+package org.example.board.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.example.board.domain.Board;
+import org.example.board.mapper.BoardMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Log4j
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+    private final BoardMapper boardMapper;
+
+    @Override
+    public List<Board> getList() {
+        return boardMapper.getList();
+    }
+
+    @Override
+    public Board detail(Long id) {
+        return boardMapper.detail(id);
+    }
+
+    @Override
+    public void create(Board board) {
+        boardMapper.create(board);
+    }
+
+    @Override
+    public Board update(Board board) {
+        boardMapper.update(board);
+        System.out.println(board.toString());
+        return detail(board.getId());
+    }
+
+    @Override
+    public Board delete(Long id) {
+        Board board = detail(id);
+        boardMapper.delete(id);
+        return board;
+    }
+}
