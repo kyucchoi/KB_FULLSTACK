1322419a5db844c1940de8ba0bef69d3566226f3
kyucchoi <kyucchoi@naver.com>
2024-09-18 11:31:53 +0900

    20240918

---
+++ EXTRA/selfStudy/src/level0/짝수홀수개수/Solution.java
@@ -0,0 +1,18 @@
+package level0.짝수홀수개수;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int num : num_list) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        return new int[]{evenCount, oddCount};
+    }
+}
