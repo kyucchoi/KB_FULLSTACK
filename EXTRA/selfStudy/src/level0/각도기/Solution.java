379922c6b6d4cb23a8a543e98fab65219cf2d5db
kyucchoi <kyucchoi@naver.com>
2024-10-06 01:12:29 +0900

    20241006

---
+++ EXTRA/selfStudy/src/level0/각도기/Solution.java
@@ -0,0 +1,15 @@
+package level0.각도기;
+
+public class Solution {
+    public int solution(int angle) {
+        if (angle < 90) {
+            return 1;  // 예각
+        } else if (angle == 90) {
+            return 2;  // 직각
+        } else if (angle < 180) {
+            return 3;  // 둔각
+        } else {
+            return 4;  // 평각
+        }
+    }
+}
