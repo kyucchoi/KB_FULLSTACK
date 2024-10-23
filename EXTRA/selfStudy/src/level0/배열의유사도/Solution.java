d2fa3aeb20c510c19fcf11e6b000a6e52bc61797
kyucchoi <kyucchoi@naver.com>
2024-10-24 02:29:03 +0900

    20241024

---
+++ EXTRA/selfStudy/src/level0/배열의유사도/Solution.java
@@ -0,0 +1,17 @@
+package level0.배열의유사도;
+
+public class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int count = 0;
+
+        for (String str1 : s1) {
+            for (String str2 : s2) {
+                if (str1.equals(str2)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
