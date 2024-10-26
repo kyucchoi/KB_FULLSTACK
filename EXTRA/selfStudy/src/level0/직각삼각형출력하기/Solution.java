a2e1bb1efe2507867c8d453a59eceadf812d8835
kyucchoi <kyucchoi@naver.com>
2024-10-26 12:35:18 +0900

    20241026

---
+++ EXTRA/selfStudy/src/level0/직각삼각형출력하기/Solution.java
@@ -0,0 +1,14 @@
+package level0.직각삼각형출력하기;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("*".repeat(i));
+        }
+    }
+}
