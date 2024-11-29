f7a2c9c43d8273198bf53a9b71992b8f290fd5ed
kyucchoi <kyucchoi@naver.com>
2024-11-29 09:37:32 +0900

    20241129

---
+++ EXTRA/selfStudy/src/level0/문자열붙여서출력하기/Solution.java
@@ -0,0 +1,13 @@
+package level0.문자열붙여서출력하기;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        System.out.print(str1 + str2);
+    }
+}
