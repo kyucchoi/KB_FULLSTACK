269569285823216fceb27321c98b121f267cac6e
kyucchoi <kyucchoi@naver.com>
2024-12-13 15:23:10 +0900

    20241213

---
+++ EXTRA/selfStudy/src/level0/n보다커질때까지더하기/Solution.java
@@ -0,0 +1,16 @@
+package level0.n보다커질때까지더하기;
+
+public class Solution {
+    public int solution(int[] numbers, int n) {
+        int sum = 0;
+
+        for (int num : numbers) {
+            sum += num;
+            if (sum > n) {
+                return sum;
+            }
+        }
+
+        return sum;
+    }
+}
