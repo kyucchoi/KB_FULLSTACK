e381eb9448372088f68442c50a3102af9e822a97
kyucchoi <kyucchoi@naver.com>
2024-12-18 22:53:00 +0900

    20241218

---
+++ EXTRA/selfStudy/src/level0/접두사인지확인하기/Solution.java
@@ -0,0 +1,7 @@
+package level0.접두사인지확인하기;
+
+public class Solution {
+    public int solution(String my_string, String is_prefix) {
+        return my_string.startsWith(is_prefix) ? 1 : 0;
+    }
+}
